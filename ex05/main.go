f64753f632550e4e89eb9c08badbac3c52ef5c0f
maxchichar <maxwellchibuezecharles@gmail.com>
2026-06-25 11:37:37 +0100

    Built a /status endpoint that accepts a code query parameter containing any HTTP status code number. The server must respond using that exact status code.

--- ex05/main.go
+++ ex05/main.go
@@ -8,5 +8,35 @@ import(
 )
 
 func statusHandler(w http.ResponseWriter, r *http.Request)  {
-	
+	code := r.URL.Query().Get("code") // Reading the code query parameter
+	// If code is empty return error 400
+	if code == ""{
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("code parameter is required"))
+		return
+	}
+
+	statusCode, err := strconv.Atoi(code) // Conversion of code to integer
+	// Error handling for invalid code
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("code must be a valid integer"))
+		return
+	}
+
+	if statusCode < 100 || statusCode > 599 { // Code must be between 100 and 599
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("code must be valid a HTTP status code (100-599)"))
+		return
+	}
+
+	w.WriteHeader(statusCode)
+	fmt.Fprintf(w, "Responding with status %d", statusCode) // Expected to provide the code
+}
+
+// Server Hosting at :8080
+func main()  {
+	http.HandleFunc("/status", statusHandler)
+	fmt.Println("Server running at http://localhost:8080/status")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
